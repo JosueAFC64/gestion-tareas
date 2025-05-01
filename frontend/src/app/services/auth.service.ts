dab27274407b5d11004847bc36a63b69e1ca23b6
Angie Aparco <angie.huarizueca123@gmail.com>
2025-05-01 13:37:56 -0500

    Refactorizar la página de inicio de sesión y agregar la integración de Angular Material

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly apiUrl = 'http://localhost:8080/api/v1/auth';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data, {
+      withCredentials: true
+    });
+  }
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  test(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/test`, {
+      withCredentials: true
+    });
+  }
 }
